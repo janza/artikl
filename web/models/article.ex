9ec10c99a6415a333b0cd66bc040ca3974824c94
Josip Janzic <josip@jjanzic.com>
2017-05-28 23:51:38 +0200

    Validate url

--- web/models/article.ex
+++ web/models/article.ex
@@ -11,6 +11,15 @@ defmodule Artikl.Article do
     timestamps()
   end
 
+  def validate_url(changeset, field, options \\ []) do
+    validate_change changeset, field, fn _, url ->
+      case url |> String.to_char_list |> :http_uri.parse do
+        {:ok, _} -> []
+        {:error, msg} -> [{field, options[:message] || "invalid url: #{inspect msg}"}]
+      end
+    end
+  end
+
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
@@ -18,5 +27,6 @@ defmodule Artikl.Article do
     struct
     |> cast(params, [:url, :title, :read, :content, :html])
     |> validate_required([:url, :read])
+    |> validate_url(:url, message: "URL is not valid")
   end
 end
