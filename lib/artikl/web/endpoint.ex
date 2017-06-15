ac5a97f820d5ec730fec8e1ea56f932212d91b8d
Josip Janzic <josip@jjanzic.com>
2017-06-15 22:11:19 +0200

    Serve favicons

--- lib/artikl/web/endpoint.ex
+++ lib/artikl/web/endpoint.ex
@@ -9,7 +9,7 @@ defmodule Artikl.Web.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :artikl, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon-32x32.png apple-touch-icon.png safari-pinned-tab.svg favicon-16x16.png favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
